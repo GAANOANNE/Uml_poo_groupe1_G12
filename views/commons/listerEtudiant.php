ad7b6c8ca9044c76a48d5ab32584c31034e0ab32
seydimouhamed <seydinabasse@gmail.com>
2020-06-27 18:06:42 +0000

    projet gestion campus

--- views/commons/listerEtudiant.php
+++ views/commons/listerEtudiant.php
@@ -3,13 +3,18 @@
 <br>
 </p>
 <span class="h4 col_princ1">Liste des étudiants</span>
-<div class="col-8 offset-2 text-center">
+<div class="col-10 offset-1 text-center">
 	<table class="table table-bordered">
     <tr>
+      <th class="">Matricule</th>
       <th class="">Nom</th>
       <th class="">Prenom</th>
-      <th class="">Telephoe</th>
-      <th class="">Matricule</th>
+      <th class="">Date Nais.</th>
+      <th class="">Phone</th>
+      <th class="">Email</th>
+      <th class="">Adresse</th>
+      <th class="">Bourse</th>
+      <th class="">Chambre</th>
     </tr>
   </table>
 </div>
